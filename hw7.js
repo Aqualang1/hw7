ab3488b43668f57eed8dc7f727f9c9781f2447a9
Ivan <ivanshvetcov@gmail.com>
2023-05-15 22:51:46 +0300

    first commit

---
+++ hw7.js
@@ -0,0 +1,9 @@
+const characters = 'abcdefghijklomnoqrstuvwyz1234567890';
+
+function generateKey(passLength, characters) {
+    const splitted = characters.split('');
+    let result = [];
+    for (let i = 0; i < passLength; i++) {
+        result.push(splitted[Math.floor(Math.random() * splitted.length)])
+    } return console.log(result.join(''));
+}
